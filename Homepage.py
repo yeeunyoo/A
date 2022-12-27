7f04c932d984bbe0aac3c519689fa413b7ee57fe
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2022-12-27 16:03:49 -0500

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -59,10 +59,9 @@ def init_connection():
     connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+st.secrets["server"]+';DATABASE='+st.secrets["database"]+';UID='+st.secrets["username"]+';PWD='+ st.secrets["password"]
     connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
     engine = create_engine(connection_url)
-    return engine
+    return engine.connect()
 
-conn = init_connection().connect()
-
+conn = init_connection()
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
