08d7cb66f5243c3f7e8601ad509808f5df9f5287
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2022-12-27 16:07:16 -0500

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -56,10 +56,16 @@ import pyodbc
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 def init_connection():
-    connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+st.secrets["server"]+';DATABASE='+st.secrets["database"]+';UID='+st.secrets["username"]+';PWD='+ st.secrets["password"]
-    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-    engine = create_engine(connection_url)
-    return engine.connect()
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
 
 conn = init_connection()
 # Perform query.
