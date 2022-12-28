dd54bba5e549f1908e74b4dd3d5fa89c2b2a6feb
yeeunyoo <116202494+yeeunyoo@users.noreply.github.com>
2022-12-28 12:40:29 -0500

    Update IVY_1.py

--- IVY_1.py
+++ IVY_1.py
@@ -48,9 +48,9 @@ st.write("if you have any question, please contact IVY SOM team")
 # %%
 
 server = '10.1.3.25' # to specify an alternate port
-database = 'KIRA' 
-username = 'kiradba' 
-password = 'Kiss!234!' 
+database = KIRA
+username = kiradba 
+password = Kiss!234! 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 cursor.execute("select distinct(salesteam_text) from [[dbo]].[TEMPORARY]]] order by salesteam_text ascending")
